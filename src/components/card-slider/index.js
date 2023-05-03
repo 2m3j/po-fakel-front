c804a78550f4eedc265853f596ee9f9ddb3addb3
Мухаммаджон Набиев <nabievmuhammad1812@mail.ru>
2023-05-03 10:19:13 +0500

    validate

--- src/components/card-slider/index.js
+++ src/components/card-slider/index.js
@@ -1,60 +1,66 @@
-new Swiper(".main-names .card-slider", {
-  loop: true,
-  slidesPerView: 1,
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+if (document.querySelector('.main-names .card-slider')) {
+  new Swiper(".main-names .card-slider", {
+    loop: true,
+    slidesPerView: 1,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 40,
+      },
     },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 20,
+    grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-right",
+      prevEl: ".swiper-button-left",
     },
-    992: {
-      slidesPerView: 4,
-      spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1200: {
-      slidesPerView: 5,
-      spaceBetween: 40,
+  });
+}
+if (document.querySelector('.aboutus__slider')) {
+  new Swiper(".aboutus__slider", {
+    loop: true,
+    slidesPerView: 1,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-  },
-  grabCursor: true,
-  navigation: {
-    nextEl: ".swiper-button-right",
-    prevEl: ".swiper-button-left",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-new Swiper(".aboutus__slider", {
-  loop: true,
-  slidesPerView: 1,
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
-new Swiper(".swiper-volonteer", {
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-right",
-    prevEl: ".swiper-button-left",
-  },
-  /*   slidesPerView: auto, */
-  slidesPerView: 1,
-  grabCursor: true,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+}
+if (document.querySelector('.swiper-volonteer')) {
+  new Swiper(".swiper-volonteer", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-right",
+      prevEl: ".swiper-button-left",
+    },
+    /*   slidesPerView: auto, */
+    slidesPerView: 1,
+    grabCursor: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
