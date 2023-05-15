6bf0dae86fa83a56d0a5b99961e693a0e50f6b8d
Alex Melkov <alex.matvey93@gmail.com>
2023-05-15 14:05:37 +0700

    change css-minimizer-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,95 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'),
-    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-    CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const path = {
-    src: {
-        blocks: 'assets/blocks',
-        js: __dirname + '/src/js/',
-        scss: __dirname + '/src/scss/',
-        templates: __dirname + '/src/templates/',
-    },
-    production: {
-        base: __dirname + '/www/',
-    }
+  src: {
+    blocks: 'assets/blocks',
+    js: __dirname + '/src/js/',
+    scss: __dirname + '/src/scss/',
+    templates: __dirname + '/src/templates/',
+  },
+  production: {
+    base: __dirname + '/www/',
+  },
 };
 
-
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: [path.src.js + 'app.js', path.src.scss + 'app.scss'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                options: {
-                    compact: false,
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {loader: "css-loader", options: {url: false}},
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name][ext]'
-                }
-            },
+  mode: 'development',
+  entry: {
+    app: [path.src.js + 'app.js', path.src.scss + 'app.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          compact: false,
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false } },
+          'sass-loader',
         ],
-    },
+      },
+    ],
     optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ],
+      minimizer: [new CssMinimizerPlugin()],
     },
     output: {
-        path: path.production.base,
-        filename: 'js/[name].bundle.js'
+      path: path.production.base,
+      filename: 'js/[name].bundle.js',
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/[name].css"
-        }),
-        new HtmlWebpackPlugin({
-            filename: path.production.base + 'index.html',
-            template: path.src.templates + "index.html",
-            inject: true
-        }),
-        new HtmlWebpackPlugin({
-            filename: path.production.base + 'about.html',
-            template: path.src.templates + "about.html",
-            inject: true
-        }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+      }),
+      new HtmlWebpackPlugin({
+        filename: path.production.base + 'index.html',
+        template: path.src.templates + 'index.html',
+        inject: true,
+      }),
+      new HtmlWebpackPlugin({
+        filename: path.production.base + 'about.html',
+        template: path.src.templates + 'about.html',
+        inject: true,
+      }),
     ],
-    devServer: {
-        static: path.production.base,
-    },
-}
+  },
+  output: {
+    path: path.production.base,
+    filename: 'js/[name].bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.production.base + 'index.html',
+      template: path.src.templates + 'index.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.production.base + 'about.html',
+      template: path.src.templates + 'about.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.production.base + 'photo-album.html',
+      template: path.src.templates + 'photo-album.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.production.base + 'photo-album-specific.html',
+      template: path.src.templates + 'photo-album-specific.html',
+      inject: true,
+    }),
+  ],
+  devServer: {
+    static: path.production.base,
+  },
+};
